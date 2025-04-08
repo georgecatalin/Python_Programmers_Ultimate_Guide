78bbcdcdfb2fa29f8f5d26a6b0b20a802f7590de
George Calin <george.calin@chorus.ro>
2025-04-08 12:04:11 +0300

    added basis examples of using strings with Python

--- Python Language Basics/String_1/main.py
+++ Python Language Basics/String_1/main.py
@@ -1,1 +1,27 @@
-print("Strings - examples 1")
+print("Strings - examples 1") # this is an example of a simple string
+
+# string concatenation
+print("hello" + "world")
+
+# multiline strings
+print("""
+this is a
+multiline string
+and it prints out exactly :->
+as it is
+""")
+
+#  special characters
+print("\nHello \tWorld Python")
+
+# string multiplication
+print("Hello, Mara" * 10)
+
+# how to escape quotations
+print("Hello 'George' ")
+print('Hello "George" ')
+print("Hello \"George\"")
+print("Hello ""George"" ")
+
+# raw strings => they are printed exactly as they are, escape characters are not accounted for
+print(r"\nThis is a \"raw string" )

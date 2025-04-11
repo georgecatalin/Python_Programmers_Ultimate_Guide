7154266e226eaf8a17f0b310c41bb67f19aec9ea
George Calin <george.calin@chorus.ro>
2025-04-11 12:27:25 +0300

    number systems in Python

---
+++ Python Language Basics/Language_Fundamentals/number_systems.py
@@ -0,0 +1,7 @@
+
+number_1 = 1978
+binary_converted_number_1 = bin(number_1)
+print("Binary converted number {} \t is \t {}".format(number_1,binary_converted_number_1))
+
+octal_converted_number_1 = oct(number_1)
+print("Octal converted number {} \t is \t {}".format(number_1,octal_converted_number_1))

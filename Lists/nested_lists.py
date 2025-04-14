ba485494cf2eece88fbaec02c2a092e7a44dd398
George Calin <george.calin@chorus.ro>
2025-04-14 15:57:04 +0300

    nested lists in Python

---
+++ Lists/nested_lists.py
@@ -0,0 +1,9 @@
+this_list = [
+    ["Maths", "History", "Physics", "Chemistry"],
+    ["English", "French", "Philosophy"],
+    ["Logic", "Biology", "Arts"]
+]
+
+print("The initial list is \t ->" , this_list)
+
+print("An element from the list -> \t" , this_list[1][2]) # "Philosophy"

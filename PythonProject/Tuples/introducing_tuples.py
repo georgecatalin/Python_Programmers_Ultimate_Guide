042d33c3889f8bd9194a612099915e8360b15c97
George Calin <george.calin@chorus.ro>
2025-04-15 09:35:19 +0300

    nested tuples in Python

--- PythonProject/Tuples/introducing_tuples.py
+++ PythonProject/Tuples/introducing_tuples.py
@@ -12,3 +12,21 @@ print(record_one_tuple[1]) # "Ward"
 
 # as the tuple is immutable , one can not modify elements of the tuple, one can not add new elements to the tuple, and one can not remove elements of the tuple
 # record_one_tuple[2] = "Something"
+
+# how to create an empty typle
+this_tuple = ()
+this_tuple_1 = tuple()
+
+print(type(this_tuple))
+print(type(this_tuple_1))
+
+# how to create a tuple hosting a single element
+single_element_tuple = ("George") # note that the parenthesis around the string, does not make it a tuple,they can be ommitted
+print(type(single_element_tuple)) # <class 'str'>
+
+single_element_tuple_real = ("George",)
+print(type(single_element_tuple_real)) # <class 'tuple'>
+
+single_element_tuple_constructor = tuple("Mara")
+print(single_element_tuple_constructor)
+print(type(single_element_tuple_constructor))
---
+++ PythonProject/Tuples/nested_tuples.py
@@ -0,0 +1,5 @@
+person_record = (1,"Jeffrey	Freddi","jfreddi0@apple.com	Male","30.162.129.18",("512-466-1862","695-920-1915"))
+
+print("The tuple \t -> ", person_record)
+print("The nested tuple \t ->", person_record[4])
+print("The mobile number \t ->", person_record[4][1])

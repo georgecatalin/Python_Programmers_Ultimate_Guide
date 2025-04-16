832015f10e77d93ef0562af3114f6acff68ad1b0
George Calin <george.calin@gmail.com>
2025-04-16 15:32:20 +0300

    introducing if-else-if expressions in Python

---
+++ If/if_else.py
@@ -0,0 +1,6 @@
+mark = 90
+
+if mark >= 50:
+    print("Congratulations, you passed") # indentation with at least 4 spaces to mark the block statements
+else:
+    print("You gotta work harder, pal")
---
+++ If/if_expression.py
@@ -0,0 +1,15 @@
+result_1 = None # Not specified
+
+mark = 89
+
+if mark >= 50:
+    result_1 = "Passed"
+else:
+    result_1 = "Failed"
+
+print(result_1)
+
+# with If expression, write the same
+
+result_2 = "Passed" if mark >= 50 else "Failed"
+print(result_2)

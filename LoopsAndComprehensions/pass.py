ed7d1a6df5ecb1560b6a0935a68cf336d9d4416c
George Calin <george.calin@gmail.com>
2025-04-22 12:44:47 +0300

    using of 'pass' inside loops of Python

---
+++ LoopsAndComprehensions/pass.py
@@ -0,0 +1,8 @@
+"""
+- whenever you wish to write a loop , but you do not want to introduce anything in the loop cause you are going to add there later
+or another programmer will, the 'pass' keyword does it for you
+- if you would not enter 'pass' , then the Python interpreter would fetch an error
+"""
+
+for i in range(5): # 0 1 2 3 4
+    pass

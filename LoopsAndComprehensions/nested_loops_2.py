ecd404e52c574b9812e5ea98a00f553de4739db1
George Calin <george.calin@gmail.com>
2025-04-22 15:07:34 +0300

    nested loops in Python

--- LoopsAndComprehensions/nested_loops_2.py
+++ LoopsAndComprehensions/nested_loops_2.py
@@ -3,3 +3,11 @@ for i in range(1,8): #  1 2 3 4 5 6 7
     for j in range(1,i+1):
         print(j, end=" ")
     print()
+
+print()
+print("*********")
+
+for i in range(1,6): #  1 2 3 4 5
+    for j in range(5,i-1,-1):
+        print(j, end=" ")
+    print()

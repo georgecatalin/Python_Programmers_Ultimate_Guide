a6875eb52e0383a22f387564713667177550d730
George Calin <george.calin@gmail.com>
2025-04-22 14:06:24 +0300

    while loop in Python

---
+++ LoopsAndComprehensions/while_loop_known_counter.py
@@ -0,0 +1,16 @@
+counter_up = 0
+
+print("\n counting upwards:")
+
+while counter_up <= 5:
+    print("The counter's value is {}".format(counter_up))
+    counter_up +=1
+
+print("\n\n counting downwards: ")
+
+counter_down = 5
+
+while counter_down >= 0:
+    print("The counter's value is {}".format(counter_down))
+    counter_down -= 1
+
---
+++ LoopsAndComprehensions/while_loop_unknown_counter.py
@@ -0,0 +1,22 @@
+repeat = True
+
+while repeat:
+    print("Food menu:")
+    print("1.Pizza con tutto")
+    print("2.Ice cream")
+    print("3.Spaghetti")
+    print("0.Exit")
+
+    user_choice = int(input("Enter your humble food choice: "))
+
+    if user_choice == 1:
+        print("Pizza is great")
+    elif user_choice == 2:
+        print("Ice cream rulz")
+    elif user_choice == 3:
+        print("Im lovin Spaghetti")
+    elif user_choice == 0:
+        # break
+        repeat = False
+    else :
+        print("I do not know what you wish")

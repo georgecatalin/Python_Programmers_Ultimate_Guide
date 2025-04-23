4f4b85a399bf0cd3577993d5af886bb19265f7a6
George Calin <george.calin@gmail.com>
2025-04-23 14:29:29 +0300

    introducing functions in Python

---
+++ Functions/introducing_functions.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("*" * 30)
+    print("one")
+    print("two")
+    print("*" * 30)
+
+my_function() # appeal the running of the function
+print("three")
+print("four")
+my_function()

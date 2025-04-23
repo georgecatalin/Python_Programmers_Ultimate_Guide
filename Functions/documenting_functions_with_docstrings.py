53d24e770d3d7d7336f4c2c37c85266ff93b5d10
George Calin <george.calin@gmail.com>
2025-04-23 14:36:24 +0300

    introducing docstrings (documentation strings) to describe functions in Python)

---
+++ Functions/documenting_functions_with_docstrings.py
@@ -0,0 +1,10 @@
+# docstrings are used to document the usage and information about the created functions in Python
+# additionally, when the developer hovers the mouse over the function, this description is readable
+
+def my_function():
+    """
+    This is the place where docstrings should be entered (documentation strings)
+    """
+    print("Hello, World")
+
+my_function()

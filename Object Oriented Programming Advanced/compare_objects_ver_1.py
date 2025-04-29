05c2af482d09e34124174c92535e64780b89f2a5
George Calin <george.calin@gmail.com>
2025-04-29 14:33:34 +0300

    compare objects in Python

---
+++ Object Oriented Programming Advanced/compare_objects_ver_1.py
@@ -0,0 +1,35 @@
+# There might arise the need to compare objects in Python , but there is not such method, and it must be created
+
+class Message:
+    """
+    This is the model of a Message
+    """
+    def __init__(self, message, date_of_transmission, sender):
+        self.message = message
+        self.data_of_transmission = date_of_transmission
+        self.sender = sender
+
+messages_list = [
+    Message("Hello", "2025-05-01 14:35", "George"),
+    Message("How are you", "2025-04-01 17:35", "Mara"),
+    Message("Thanks", "2025-12-14 10:35", "Sorina"),
+]
+
+# there appears a new message. one needs to compare it with existing ones to see whether to add it to the list or not
+
+new_message = Message("Hello", "2024-12-31 23:45", "George")
+
+is_found = False
+
+for message in messages_list:
+    if message.message == new_message.message and message.sender == new_message.sender:
+        is_found = True
+
+if not is_found:
+    print("The message was not found and will be added to the list")
+    messages_list.append(new_message)
+
+print(messages_list)
+
+for message in messages_list:
+    print(vars(message))
